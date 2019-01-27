ea687f971a82b3899ed8556e11b306e6c40f3d09
Avi Miller <TrebecS@gmail.com>
2019-01-26 18:47:18 -0800

    Add files via upload

---
+++ Assets/Scripts/CritterControl.cs
@@ -0,0 +1,231 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CritterControl : MonoBehaviour
+{
+//================================================================================
+// Required References
+//================================================================================
+
+    [SerializeField] CritterHead testHead;
+
+//================================================================================
+// Behavior Variables
+//================================================================================
+
+    [Space(10)]
+    [SerializeField] float maxLiftTime = 3f;
+    [SerializeField] float maxForwardTime = 3f;
+    [SerializeField] float maxBackTime = 3f;
+    [SerializeField] float maxDropTime = 3f;
+    [SerializeField] float verticalHaltThreshold = 0.1f;
+    [SerializeField] float horizontalHaltThreshold = 0.03f;
+
+//================================================================================
+// Movement State Enum
+//================================================================================
+
+    enum MoveState {
+        LeftLegUp, LeftLegDown, LeftLegIn, LeftLegOut,
+        RightLegUp, RightLegDown, RightLegIn, RightLegOut
+    }
+
+//================================================================================
+// Local Variables
+//================================================================================
+
+    int orderCount = 8;
+    MoveState[] moveOrderLeft = { MoveState.LeftLegUp, MoveState.LeftLegOut, MoveState.LeftLegDown, MoveState.LeftLegIn,
+                            MoveState.RightLegUp, MoveState.RightLegIn, MoveState.RightLegDown, MoveState.RightLegOut};
+    MoveState[] moveOrderRight = { MoveState.RightLegUp, MoveState.LeftLegOut, MoveState.RightLegOut, MoveState.RightLegDown,
+                            MoveState.LeftLegUp, MoveState.RightLegIn, MoveState.LeftLegIn, MoveState.LeftLegDown};
+    bool moveRight = true;
+    int orderIndex = 0;
+    float timer = 0f;
+
+//================================================================================
+// Code Control
+//================================================================================
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //ManualControl();
+        AutoControl();
+    }
+
+    void LegMotion(float input, CritterHead.Side side) {
+        if (System.Math.Abs(input) < Mathf.Epsilon) {
+            testHead.SetLockJoint(side, true);
+            testHead.SetJointMotor(side, CritterHead.MotorState.Off);
+        } else {
+            testHead.SetLockJoint(side, false);
+            CritterHead.MotorState state;
+            if (input > 0) {
+                state = CritterHead.MotorState.Extend;
+            } else {
+                state = CritterHead.MotorState.Retract;
+            }
+            testHead.SetJointMotor(side, state);
+        }
+    }
+
+//================================================================================
+// Walk Behavior
+//================================================================================
+
+    void AutoControl() {
+        Walk();
+    }
+
+    void Walk() {
+        timer += Time.fixedDeltaTime;
+        MoveState[] moveOrder = moveRight ? moveOrderRight : moveOrderLeft;
+        float progressForjoint = testHead.JointProgress(JointFromMoveState(moveOrder[orderIndex]));
+
+        //Check if the state is complete; may add additional sensors later for things like ceiling collision.
+        if (timer >= MaxTimeForState(moveOrder[orderIndex], moveRight)) {
+            print("Timed out on: " + moveOrder[orderIndex]);
+            IncrementState();
+        } else if (ThresholdFinished(moveOrder[orderIndex], progressForjoint)) {
+            print("Finished movement on: " + moveOrder[orderIndex]);
+            IncrementState();
+        }
+
+        ExecuteMotion(moveOrder[orderIndex]);
+    }
+
+    void ExecuteMotion(MoveState state) {
+        switch (state) {
+            case MoveState.LeftLegDown:
+                LegMotion(1f, CritterHead.Side.LeftVertical);
+                break;
+            case MoveState.LeftLegIn:
+                LegMotion(-1f, CritterHead.Side.LeftHorizontal);
+                break;
+            case MoveState.LeftLegUp:
+                LegMotion(-1f, CritterHead.Side.LeftVertical);
+                break;
+            case MoveState.LeftLegOut:
+                LegMotion(1f, CritterHead.Side.LeftHorizontal);
+                break;
+            case MoveState.RightLegIn:
+                LegMotion(-1f, CritterHead.Side.RightHorizontal);
+                break;
+            case MoveState.RightLegUp:
+                LegMotion(-1f, CritterHead.Side.RightVertical);
+                break;
+            case MoveState.RightLegDown:
+                LegMotion(1f, CritterHead.Side.RightVertical);
+                break;
+            default:
+                LegMotion(1f, CritterHead.Side.RightHorizontal);
+                break;
+        }
+    }
+
+    void IncrementState() {
+        Halt();
+        orderIndex = (orderIndex + 1) % orderCount;
+        timer = 0f;
+    }
+
+    void Halt() {
+        LegMotion(0f, CritterHead.Side.LeftHorizontal);
+        LegMotion(0f, CritterHead.Side.LeftVertical);
+        LegMotion(0f, CritterHead.Side.RightHorizontal);
+        LegMotion(0f, CritterHead.Side.RightVertical);
+    }
+
+//================================================================================
+// Converters
+//================================================================================
+
+    CritterHead.Side JointFromMoveState(MoveState state) {
+        switch (state) {
+            case MoveState.LeftLegOut:
+            case MoveState.LeftLegIn:
+                return CritterHead.Side.LeftHorizontal;
+            
+            case MoveState.LeftLegUp:
+            case MoveState.LeftLegDown:
+                return CritterHead.Side.LeftVertical;
+            
+            case MoveState.RightLegIn:
+            case MoveState.RightLegOut:
+                return CritterHead.Side.RightHorizontal;
+            
+            default:
+                return CritterHead.Side.RightVertical;
+        }
+    }
+
+    /// <summary>
+    /// Returns whether the distance threshold has been reached under the given criteria
+    /// </summary>
+    /// <returns><c>true</c>, if finished was thresholded, <c>false</c> otherwise.</returns>
+    /// <param name="state">State.</param>
+    /// <param name="progress">Progress.</param>
+    bool ThresholdFinished(MoveState state, float progress) {
+        if(state == MoveState.LeftLegOut || state == MoveState.LeftLegIn)
+            print("Progress for " + state + ": " + progress);
+        switch (state) {
+            case MoveState.LeftLegUp:
+            case MoveState.RightLegUp:
+                return progress < verticalHaltThreshold;    //progress is smaller as leg rises; when less than threshold -> finished
+            case MoveState.LeftLegOut:
+            case MoveState.RightLegOut:
+                return progress > 1f - horizontalHaltThreshold;   //progress is larger as leg moves away
+            case MoveState.LeftLegIn:
+            case MoveState.RightLegIn:
+                return progress < (float)horizontalHaltThreshold;
+            default:
+                return progress > 1f - verticalHaltThreshold;
+        }
+    }
+
+    /// <summary>
+    /// Returns the maximum time allowed for the given state.
+    /// </summary>
+    /// <returns>The time for state.</returns>
+    /// <param name="state">State.</param>
+    /// <param name="moveRight">If set to <c>true</c> move right.</param>
+    float MaxTimeForState(MoveState state, bool moveRight) {
+        switch (state) {
+            case MoveState.LeftLegUp:
+            case MoveState.RightLegUp:
+                return maxLiftTime;
+            case MoveState.LeftLegOut:
+            case MoveState.RightLegIn:
+                return moveRight ? maxBackTime : maxForwardTime;
+            case MoveState.LeftLegIn:
+            case MoveState.RightLegOut:
+                return moveRight ? maxForwardTime : maxBackTime;
+            default:
+                return maxDropTime;
+        }
+    }
+
+//================================================================================
+// Debugging
+//================================================================================
+
+    void ManualControl() {
+        float leftFootDown = Input.GetAxis("Vertical");
+        float rightFootDown = Input.GetAxis("Second Vertical");
+        float leftFootOut = Input.GetAxis("Horizontal");
+        float rightFootOut = Input.GetAxis("Second Horizontal");
+
+        LegMotion(leftFootOut, CritterHead.Side.LeftHorizontal);
+        LegMotion(leftFootDown, CritterHead.Side.LeftVertical);
+        LegMotion(rightFootOut, CritterHead.Side.RightHorizontal);
+        LegMotion(rightFootDown, CritterHead.Side.RightVertical);
+    }
+}
---
+++ Assets/Scripts/CritterHead.cs
@@ -0,0 +1,237 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CritterHead : MonoBehaviour
+{
+
+//==============================================================================
+// Enums
+//==============================================================================
+
+    public enum Side {
+        LeftHorizontal, LeftVertical, RightHorizontal, RightVertical
+    }
+
+    public enum MotorState {
+        Extend, Retract, Off
+    }
+
+//==============================================================================
+// Required references
+//==============================================================================
+
+    [SerializeField] SliderJoint2D leftHorizontalJoint;
+    [SerializeField] SliderJoint2D leftVerticalJoint;
+    [SerializeField] SliderJoint2D rightHorizontalJoint;
+    [SerializeField] SliderJoint2D rightVerticalJoint;
+
+    [Space(10)]
+    [SerializeField] FixedJoint2D leftHorizontalLock;
+    [SerializeField] FixedJoint2D leftVerticalLock;
+    [SerializeField] FixedJoint2D rightHorizontalLock;
+    [SerializeField] FixedJoint2D rightVerticalLock;
+
+//==============================================================================
+// Adjustment variables
+//==============================================================================
+
+    [Space(10)]
+    [SerializeField] float horizontalMax = 1.5f;
+    [SerializeField] float horizontalMin = 0.5f;
+    [SerializeField] float critterStrength = 50f;
+    [SerializeField] float targetSpeed = 1f;
+
+    float jointLockTolerance = 0.01f;
+
+//==============================================================================
+// Local variables
+//==============================================================================
+
+    JointTranslationLimits2D leftVerticalLimits;
+    JointTranslationLimits2D rightVerticalLimits;
+    //the horizontal limits are always the same
+
+//==============================================================================
+// Sensors
+//==============================================================================
+
+    /// <summary>
+    /// Returns the progress, in percent of total distance, that the object has moved
+    /// along the joint path. Note that the value increases DOWN and IN and decreases
+    /// OUT and UP.
+    /// </summary>
+    /// <returns>The progress.</returns>
+    /// <param name="side">Side.</param>
+    public float JointProgress(Side side) {
+        SliderJoint2D joint = SideToSlideJoint(side);
+        return Mathf.InverseLerp(joint.limits.min, joint.limits.max, joint.jointTranslation);
+    }
+
+    /*
+    public bool LimbContact(Side side) {
+
+    }
+    */   
+
+//==============================================================================
+// Code Control
+//==============================================================================
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftVerticalLimits = leftVerticalJoint.limits;
+        rightVerticalLimits = rightVerticalJoint.limits;
+
+        JointMotor2D defaultMotor = new JointMotor2D() {
+            maxMotorTorque = critterStrength
+        };
+        leftVerticalJoint.motor = defaultMotor;
+        leftHorizontalJoint.motor = defaultMotor;
+        rightVerticalJoint.motor = defaultMotor;
+        rightHorizontalJoint.motor = defaultMotor;
+    }
+
+    /// <summary>
+    /// Locks or unlocks the joint.
+    /// </summary>
+    /// <param name="side">Side.</param>
+    /// <param name="locked">If set to <c>true</c> locked.</param>
+    public void SetLockJoint(Side side, bool locked) {
+        SliderJoint2D joint = SideToSlideJoint(side);
+        //print(joint.jointTranslation);
+        if (locked)
+            LockJoint(side);
+        else
+            UnlockJoint(side);
+    }
+
+    /// <summary>
+    /// Changes the state of the joint motor.
+    /// For the vertical joints, Extend is down and Retract is up.
+    /// </summary>
+    /// <param name="side">Side.</param>
+    /// <param name="state">State.</param>
+    public void SetJointMotor(Side side, MotorState state) {
+        SliderJoint2D joint = SideToSlideJoint(side);
+        if (state == MotorState.Off) {
+            joint.useMotor = false;
+        } else {
+            float speed = targetSpeed * (state == MotorState.Extend ? 1 : -1);
+            JointMotor2D motor = joint.motor;
+            motor.motorSpeed = speed;
+            joint.motor = motor;
+            joint.useMotor = true;
+        }
+    }
+
+//==============================================================================
+// Locking and unlocking systems
+//==============================================================================
+
+    /// <summary>
+    /// Locks the joint on the specified side.
+    /// </summary>
+    /// <param name="side">Side.</param>
+    void LockJoint(Side side) {
+        SetLockViaFixed(side, true);
+    }
+
+    /// <summary>
+    /// Locks a slider joint connection by squeezing its limits down to near zero space.
+    /// </summary>
+    /// <param name="side">Side.</param>
+    void LockViaLimits(Side side) {
+        SliderJoint2D joint = SideToSlideJoint(side);
+        if (joint.limits.max - joint.limits.min <= jointLockTolerance * 2) {
+            print("Locked");
+            return;
+        }
+        JointTranslationLimits2D lockedLimits = new JointTranslationLimits2D {
+            min = joint.jointTranslation - jointLockTolerance,
+            max = joint.jointTranslation + jointLockTolerance
+        };
+        joint.limits = lockedLimits;
+    }
+
+    /// <summary>
+    /// Locks a slider joint by disabling it and enabling a fixed joint instead.
+    /// </summary>
+    /// <param name="side">Side.</param>
+    void SetLockViaFixed(Side side, bool locked) {
+        SliderJoint2D joint = SideToSlideJoint(side);
+        FixedJoint2D lockJoint = SideToLockJoint(side);
+
+        joint.enabled = !locked;
+        lockJoint.enabled = locked;
+    }
+
+    /// <summary>
+    /// Unlocks the joint on the specifide side.
+    /// </summary>
+    /// <param name="side">Side.</param>
+    void UnlockJoint(Side side) {
+        SetLockViaFixed(side, false);
+    }
+
+    /// <summary>
+    /// Unlocks the given joint by adjusting its limits
+    /// </summary>
+    /// <param name="side">Side.</param>
+    void UnlockViaLimits(Side side) {
+        SliderJoint2D joint = SideToSlideJoint(side);
+        if (side == Side.LeftHorizontal || side == Side.RightHorizontal) {
+            JointTranslationLimits2D unlockedLimits = new JointTranslationLimits2D {
+                max = horizontalMax,
+                min = horizontalMin
+            };
+            joint.limits = unlockedLimits;
+        } else {
+            if (side == Side.RightVertical)
+                joint.limits = rightVerticalLimits;
+            else
+                joint.limits = leftVerticalLimits;
+        }
+    }
+
+//==============================================================================
+// Converters
+//==============================================================================
+
+    /// <summary>
+    /// Returns the joint that corresponds with the given side.
+    /// </summary>
+    /// <returns>The slide joint.</returns>
+    /// <param name="side">Side.</param>
+    SliderJoint2D SideToSlideJoint(Side side) {
+        switch (side) {
+            case Side.LeftHorizontal:
+                return leftHorizontalJoint;
+            case Side.LeftVertical:
+                return leftVerticalJoint;
+            case Side.RightHorizontal:
+                return rightHorizontalJoint;
+            default:
+                return rightVerticalJoint;
+        }
+    }
+
+    /// <summary>
+    /// Returns the lock that corresponds with the given side.
+    /// </summary>
+    /// <returns>The lock joint.</returns>
+    /// <param name="side">Side.</param>
+    FixedJoint2D SideToLockJoint(Side side) {
+        switch (side) {
+            case Side.LeftHorizontal:
+                return leftHorizontalLock;
+            case Side.LeftVertical:
+                return leftVerticalLock;
+            case Side.RightHorizontal:
+                return rightHorizontalLock;
+            default:
+                return rightVerticalLock;
+        }
+    }
+}
